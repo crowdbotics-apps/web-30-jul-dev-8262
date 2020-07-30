17e6b21aa5bdaea6b3a0edb29971ce25bf5330cf
Crowdbotics <team@crowdbotics.com>
2020-07-30 05:15:41 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -6,18 +7,18 @@ from django.db import models
 
 
 class CustomText(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150,)
 
     def __str__(self):
         return self.title
 
     @property
     def api(self):
-        return f'/api/v1/customtext/{self.id}/'
+        return f"/api/v1/customtext/{self.id}/"
 
     @property
     def field(self):
-        return 'title'
+        return "title"
 
 
 class HomePage(models.Model):
@@ -25,8 +26,31 @@ class HomePage(models.Model):
 
     @property
     def api(self):
-        return f'/api/v1/homepage/{self.id}/'
+        return f"/api/v1/homepage/{self.id}/"
 
     @property
     def field(self):
-        return 'body'
+        return "body"
+
+
+class TestFieldName(models.Model):
+    "Generated Model"
+    one = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="testfieldname_one",
+    )
+    two = models.ForeignKey(
+        "home.TestFieldName",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="testfieldname_two",
+    )
+    three = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="testfieldname_three",
+    )
--- web_30_jul_dev_8262/wsgi.py
+++ web_30_jul_dev_8262/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_30_jul_dev_8262.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web_30_jul_dev_8262.settings")
 
 application = get_wsgi_application()
